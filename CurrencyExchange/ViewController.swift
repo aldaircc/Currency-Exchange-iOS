41eeced9852451dd238ce7a9f1898dcff44c5589
Aldair Cosetito Coral <aldair.soft@hotmail.com>
2020-09-14 10:47:36 -0500

    Initial Commit

---
+++ CurrencyExchange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyExchange
+//
+//  Created by Aldair Raul Cosetito Coral on 9/14/20.
+//  Copyright © 2020 Aldair Cosetito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
